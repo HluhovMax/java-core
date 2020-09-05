078355d0bee06ddf783d86eb69533d7ddbed7c80
Maksym Hlukhov <maksym.hlukhov@bitvoodoo.ch>
2020-09-06 00:34:57 +0300

    added encapsulation lesson

---
+++ src/main/java/io/mh/encapsulation/Main.java
@@ -0,0 +1,22 @@
+package io.mh.encapsulation;
+
+public class Main {
+	public static void main(String[] args) {
+		Player player = new Player();
+		player.fullName = "Max";
+		player.health = 20;
+		player.weapon = "Sword";
+
+		int damage = 10;
+
+		player.loseHealth(damage);
+		System.out.println("Remaining health = " + player.getRemaining());
+
+		damage = 11;
+		player.health = 200;
+		player.loseHealth(damage);
+		System.out.println("Remaining health = " + player.getRemaining());
+
+	}
+
+}
